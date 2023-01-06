a5045beb8466e0186eb696aaf3a7e2c839fb6604
Chetana21 <pchetana691@gmail.com>
2023-01-06 20:06:56 +0530

    alljobs

--- main.js
+++ main.js
@@ -1,58 +1,48 @@
-const express = require("express")
-const dotenv = require("dotenv")
-const mongoose = require("mongoose")
-const bodyParser = require("body-parser")
-const errorHandler = require("./middleware/errorHandler")
-const tryCatch = require("./utils/tryCatch")
-const AppError = require("./utils/AppError")
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const errorHandler = require("./middleware/errorHandler");
+const tryCatch = require("./utils/tryCatch");
+const AppError = require("./utils/AppError");
 const cookieParser = require("cookie-parser");
-const path =require('path');
-const route = require("./routes/route")
+const path = require("path");
+const route = require("./routes/route");
 dotenv.config();
 const app = express();
-const PORT = 5000 || process.env.PORT;
+const PORT = 5004 || process.env.PORT;
 
 // serving static files
-app.set('view engine','ejs');
-app.set('views',path.join(__dirname,'views'));
-app.use(express.static(__dirname + '/public'));
-
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(__dirname + "/public"));
 
 // Body-parser middleware
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cookieParser());
 
 // connecting db
-mongoose.set('strictQuery', false);
-mongoose.connect(process.env.URI,()=>{
-  console.log("Db connected")
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.URI, () => {
+  console.log("Db connected");
 });
 console.log(mongoose.connection.readyState);
 
+const getUser = () => undefined;
 
-
-
-
-const getUser = ()=> undefined;
-
-app.get('/', tryCatch( async (req,res,next)=>{
-
-    
-        res.render("landing")
-})
+app.get(
+  "/",
+  tryCatch(async (req, res, next) => {
+    res.render("landing");
+  })
 );
-
 
 //middleware
 app.use(errorHandler);
-app.use(route)
-
-
-
-
+app.use(route);
 
 // listining port
-app.listen(PORT,(req,res)=>{
-  console.log(`server is listining on port ${PORT}`)
+app.listen(PORT, (req, res) => {
+  console.log(`server is listining on port ${PORT}`);
 });
